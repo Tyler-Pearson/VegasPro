59f82550c25e8eb1014e1e44ace6734c84477ff5
Tyler-Pearson <tylerpearson1993@gmail.com>
2019-12-18 00:40:16 -0600

    Scrape stats, write stats to stats.csv

--- driver.py
+++ driver.py
@@ -5,13 +5,14 @@ import sys
 
 CUR_WEEK = 15
 RESULTS_FILENAME = "results.csv"
+STATS_FILENAME = "stats.csv"
 
 
 
 def add_teams(results, stats):
    for result in results:
-      stats[result[0]] = {}
-      stats[result[2]] = {}
+      stats[result[0]] = [result[0]]
+      stats[result[2]] = [result[2]]
 
 
 def write_results(results, filename):
@@ -20,6 +21,25 @@ def write_results(results, filename):
         filewriter.writerow(["home_team", "home_score", "away_team", "away_score"])
         for result in results:
             filewriter.writerow(result)
+
+# ppg, to_pg, 3/4 conv, rz_att, rz_conv
+
+# defs:
+#  pg=per game
+#  to=turnovers
+#  ld=late down
+#  cr=conversion rate
+#  rz=red zone (last 20 yards)
+#  apg=attempts per game
+#  d=defensive
+#  ta=takeaways
+def write_stats(stats, filename):
+    with open(filename, 'wb') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(["team_name", "ppg", "to_pg", "ld_cr", "rz_apg", "rz_cr",\
+                "dppg", "ta_pg", "dld_cr", "drz_apg", "drz_cr"])
+        for team in stats:
+            filewriter.writerow(stats[team])
 
 
 def main():
@@ -36,9 +56,19 @@ def main():
    # create stats dict, add dict(team)={} for each team
    print "init stats dictionary, adding base teams..."
    add_teams(results[:16], stats)
-   print str(len(stats)) + " teams added"
+   print str(len(stats)) + " teams added!"
 
+   print "getting stats..."
    add_stats(stats)
+
+   print "writing stats to " + STATS_FILENAME
+   write_stats(stats, STATS_FILENAME)
+
+   # print "---"
+   # for stat in stats:
+   #     print stat
+   #     print stats[stat]
+   #     print "---"
 
 
 if __name__=="__main__":
--- stats_scrape.py
+++ stats_scrape.py
@@ -1,51 +1,94 @@
 import re
 import urllib2
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 import sys
 
 
+OFFENSE_URL = "https://www.pro-football-reference.com/years/2019/index.htm"
+DEFENSE_URL = "https://www.pro-football-reference.com/years/2019/opp.htm"
 
-def get_week(url):
 
-   page = urllib2.urlopen(url)
-   soup = BeautifulSoup(page, 'html.parser')
-   game_summaries = soup.find('div', attrs={'class':'game_summaries'})
-   games = game_summaries.find_all('div', attrs={'class':'game_summary'})
+# Helper function, football-ref html returns tables in comments
+# Return tables from url in dict, {table_id:table}
+def get_tables(url):
+    tables = {}
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
+    noncomment_tables = soup.find_all('table')
+    for table in noncomment_tables:
+        # print table.get('id')
+        tables[str(table.get('id'))] = table
+    # NOTE: many tables are in comments, below code extracts them
+    comments = soup.find_all(string = lambda text:isinstance(text, Comment))
+    for comment in comments:
+        comment = BeautifulSoup(str(comment), 'html.parser')
+        table = comment.find('table')
+        if (table):
+            # print table.get('id')
+            tables[str(table.get('id'))] = table
+    return tables
 
-   results = []
-   for game in games:
-      result = game.find('table', attrs={'class':'teams'})
-      teams = result.find_all('tr')
-      results.append((
-         str(teams[1].find('a').text),
-         int(teams[1].find('td', attrs={'class':'right'}).text),
-         str(teams[2].find('a').text),
-         int(teams[2].find('td', attrs={'class':'right'}).text)))
 
-   return results
+# soup should be just the Team Offense table
+def add_general(stats, soup):
+    teams = soup.find('tbody').find_all('tr')
+    for team in teams:
+        name = team.find('a').text
+        games_played = float(team.find('td', attrs={'data-stat':'g'}).text)
+        # PPG
+        points_for = float(team.find('td', attrs={'data-stat':'points'}).text)
+        stats[name].append(points_for/games_played)
+        # Turnovers per game
+        turnovers = float(team.find('td', attrs={'data-stat':'turnovers'}).text)
+        stats[name].append(turnovers/games_played)
+
+
+
+# soup should be just the Team Conversions table
+def add_conversions(stats, soup):
+    teams = soup.find('tbody').find_all('tr')
+    for team in teams:
+        name = team.find('a').text
+        games_played = float(team.find('td', attrs={'data-stat':'g'}).text)
+        # Third and fourth conversion rate combined
+        third_att = float(team.find('td', attrs={'data-stat':'third_down_att'}).text)
+        third_suc = float(team.find('td', attrs={'data-stat':'third_down_success'}).text)
+        fourth_att = float(team.find('td', attrs={'data-stat':'fourth_down_att'}).text)
+        fourth_suc = float(team.find('td', attrs={'data-stat':'fourth_down_success'}).text)
+        stats[name].append((third_suc + fourth_suc)/(third_att + fourth_att))
+        # Red zone attempts and red zone TD rate
+        rz_att = float(team.find('td', attrs={'data-stat':'red_zone_att'}).text)
+        rz_suc = float(team.find('td', attrs={'data-stat':'red_zone_scores'}).text)
+        stats[name].append(rz_att/games_played)
+        stats[name].append(rz_suc/rz_att)
 
 
 # ADD OFFENSE
-# ADD DEFENSE
+def add_side(stats, url):
+    tables = get_tables(url)
+    add_general(stats, tables["team_stats"])
+    add_conversions(stats, tables["team_conversions"])
+
+
+
+# ADD ALL STATS
+# ppg, to_pg, 3/4 conv, rz_att, rz_conv
 def add_stats(stats):
 
-   results = []
+   add_side(stats, OFFENSE_URL)
+   add_side(stats, DEFENSE_URL)
 
-   for i in range(cur_week):
-      url = BASE_URL +  "week_" + str(i+1) + ".htm"
-      # print url
-      results.extend(get_week(url))
-
-   return results
+   # no need for return, stats dict is populated directly
 
 
-def main():
-
-   print "Getting results"
-   results = get_results(CUR_WEEK)
-   print str(len(results)) + " results scraped"
-   print results[0]
-
-
-if __name__=="__main__":
-   main()
+### Need "stats" dictionary of teams:[] to use this class
+# def main():
+#
+#    print "Getting stats"
+#    results = get_results(CUR_WEEK)
+#    print str(len(results)) + " results scraped"
+#    print results[0]
+#
+#
+# if __name__=="__main__":
+#    main()
